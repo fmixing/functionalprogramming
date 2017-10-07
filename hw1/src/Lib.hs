621feb4ccb562a13f61473c30409eba51d758020
fmixing <alicekoroleva239@gmail.com>
2017-10-08 00:19:10 +0300

    tests are complited

---
+++ hw1/src/Lib.hs
@@ -0,0 +1,516 @@
+module Lib
+(
+    randomIntList,
+    --Block1
+    order3,
+    highestBit,
+    highestBitHard,
+    smartReplicate,
+    contains,
+
+    --Block2
+    removeAt,
+    removeAtHard,
+    collectEvery,
+    stringSum,
+    stringSumHard,
+    split,
+    mergeSort,
+
+    --Block3
+    DaysOfWeek (..),
+    numOfDay,
+    dayFromNum,
+    nextDay,
+    afterDays,
+    isWeekend,
+    daysToParty,
+    Creature (..),
+    CreatureStats (..),
+    fighting,
+    Vector (..),
+    to3D,
+    vecLength,
+    vecSum,
+    vecMul,
+    dist,
+    vecMulVect,
+    Nat (..),
+    natSum,
+    natMul,
+    natSub,
+    natToNum,
+    isEven,
+    natDiv,
+    natMod,
+    natGcd,
+    Tree (..),
+    isEmptyTree,
+    sizeTree,
+    findElemTree,
+    insertIntoTree,
+    fromList,
+    --Block4
+    splitOn,
+    joinWith,
+    maybeConcat,
+    eitherConcat,
+    NonEmpty (..),
+    Name (..),
+    Identity (..),
+    Endo (..),
+    Arrow (..)
+) where
+
+
+import           Data.Char       (isDigit, isSpace)
+import           Text.Read       (readMaybe)
+import           Data.Either     (lefts, rights)
+import           Data.Foldable   (toList)
+import           Data.List       (sort)
+import           Data.Maybe      (fromJust, fromMaybe, isNothing)
+import           Data.Monoid     as Monoid (mempty)
+import           Data.Semigroup  as Semigroup hiding (Endo, mempty)
+import           Numeric.Natural
+import           Prelude         hiding (mempty)
+import           System.Random   (newStdGen, randomRs)
+
+randomIntList :: Int -> Int -> Int -> IO [Int]
+randomIntList n from to = take n . randomRs (from, to) <$> newStdGen
+
+------------Block1
+
+order3 :: Ord a => (a, a, a) -> (a, a, a)
+order3 (x, y, z) =
+    let [x', y', z'] = sort [x, y, z] in (x', y', z')
+
+highestBit :: Natural -> Maybe (Natural)
+highestBit x
+    | x == 0 = Nothing
+    | x == 1 = Just 1
+    | otherwise =
+        let s = highestBit (x `div` 2) in
+            (if isNothing s then s else
+                (let j = fromJust s in Just (j * 2)))
+
+highestBitHard :: Natural -> Maybe (Natural, Natural)
+highestBitHard x
+        | x == 0     = Nothing
+        | x == 1     = Just (1, 0)
+        | otherwise =
+            let s = highestBitHard (x `div` 2) in
+                (if isNothing s then s else
+                    (let j = fromJust s in Just (fst j * 2, snd j + 1)))
+
+smartReplicate :: [Int] -> [Int]
+smartReplicate = concatMap (\x -> replicate x x)
+
+contains :: Eq a => a -> [[a]] -> [[a]]
+contains e = filter $ elem e
+
+-------------Block2
+
+removeAt :: Int -> [a] -> Maybe [a]
+removeAt _ [] = Just []
+removeAt num (x : xs)
+    | num < 0       = Nothing
+    | num == 0      = Just xs
+    | otherwise     = Just ([x] ++ fromJust (removeAt (num - 1) xs))
+
+removeAtHard :: Int -> [a] -> (Maybe a, [a])
+removeAtHard _ []           = (Nothing, [])
+removeAtHard num (x : xs)
+        | num < 0       = (Nothing, x : xs)
+        | num == 0      = (Just x, xs)
+        | otherwise     =
+            let (maybeA, list) = removeAtHard (num - 1) xs in (maybeA, x : list)
+
+collectEvery :: Int -> [a] -> Maybe ([a], [a])
+collectEvery n list
+        | n < 1     = Nothing
+        | otherwise = Just $ collectEvery' 1 list
+        where
+            collectEvery' _ []       = ([], [])
+            collectEvery' i (x : xs) =
+                case i `mod` n of
+                    0 -> let (oldlist, removed) = collectEvery' (i + 1) xs in
+                           (oldlist, x : removed)
+                    _ -> let (oldlist, removed) = collectEvery' (i + 1) xs in
+                           (x : oldlist, removed)
+
+stringSum :: String -> Maybe Integer
+stringSum str = fmap sum $ sequence $ map (\x -> readMaybe x :: Maybe Integer) $ words str
+
+stringSumHard :: String -> Maybe Int
+stringSumHard = stringSumHard' []
+        where
+            stringSumHard' [] []            = Just 0
+
+            stringSumHard' strToNum []      =
+                if strToNum == ['-'] || strToNum == ['+']
+                    then Nothing
+                    else Just (read strToNum :: Int)
+
+            stringSumHard' ['+'] (x : xs)   =
+                if isDigit x
+                    then stringSumHard' [x] xs
+                    else Nothing
+
+            stringSumHard' ['-'] (x : xs)   =
+                if isDigit x
+                    then stringSumHard' ('-' : [x]) xs
+                    else Nothing
+
+            stringSumHard' strToNum (x : xs)
+                | isSpace x             =
+                    if null strToNum
+                        then stringSumHard' [] xs
+                        else (let currInt = (read strToNum :: Int) in
+                            let strSum = stringSumHard' [] xs in
+                                if isNothing strSum
+                                    then Nothing
+                                    else Just (currInt + fromJust (stringSumHard' [] xs)))
+                | x == '-' || x == '+' =
+                    if null strToNum
+                        then stringSumHard' [x] xs
+                        else Nothing
+                | not $ isDigit x       = Nothing
+                | otherwise             = stringSumHard' (strToNum ++ [x]) xs
+
+
+split :: [a] -> ([a], [a])
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:xys) = (x:xs, y:ys) where (xs, ys) = split xys
+
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [x] = [x]
+mergeSort [] = []
+mergeSort arr =
+    let mid = (length arr + 1) `div` 2 --------todo: fix splitting
+    in merge (mergeSort $ fst $ splitAt mid arr) (mergeSort $ snd $ splitAt mid arr)
+        where
+            merge xs [] = xs
+            merge [] ys = ys
+            merge (x:xs) (y:ys) =
+                if x > y
+                    then y : merge (x : xs) ys
+                    else x : merge xs (y : ys)
+
+----------------Block3
+
+data DaysOfWeek = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday deriving (Show, Eq)
+
+numOfDay :: DaysOfWeek -> Int
+numOfDay Monday    = 0
+numOfDay Tuesday   = 1
+numOfDay Wednesday = 2
+numOfDay Thursday  = 3
+numOfDay Friday    = 4
+numOfDay Saturday  = 5
+numOfDay Sunday    = 6
+
+dayFromNum :: Int -> DaysOfWeek
+dayFromNum num
+    | num == 0 = Monday
+    | num == 1 = Tuesday
+    | num == 2 = Wednesday
+    | num == 3 = Thursday
+    | num == 4 = Friday
+    | num == 5 = Saturday
+    | num == 6 = Sunday
+    | otherwise = dayFromNum (num `mod` 7)
+
+nextDay :: DaysOfWeek -> DaysOfWeek
+nextDay day = (\x -> dayFromNum (x + 1)) $ numOfDay day
+
+afterDays :: DaysOfWeek -> Int -> DaysOfWeek
+afterDays day num = (\x -> dayFromNum (x + num)) $ numOfDay day
+
+isWeekend :: DaysOfWeek -> Bool
+isWeekend day
+    | day == Saturday   = True
+    | day == Sunday     = True
+    | otherwise         = False
+
+daysToParty :: DaysOfWeek -> Int
+daysToParty Monday    = 4
+daysToParty Tuesday   = 3
+daysToParty Wednesday = 2
+daysToParty Thursday  = 1
+daysToParty Friday    = 0
+daysToParty Saturday  = 6
+daysToParty Sunday    = 5
+
+
+data Creature
+    = Knight { hp :: Int, attack :: Int }
+    | Monster { hp :: Int, attack :: Int } deriving (Show)
+
+class CreatureStats a where
+    getHp :: a -> Int
+    getAttackPoints :: a -> Int
+    decreaseHp :: a -> Int -> a
+
+instance CreatureStats Creature where
+    getHp (Knight hpBar _)  = hpBar
+    getHp (Monster hpBar _) = hpBar
+    getAttackPoints (Knight _ attackBar)  = attackBar
+    getAttackPoints (Monster _ attackBar) = attackBar
+    decreaseHp (Knight hpBar attackBar) healthPoints = Knight (hpBar - healthPoints) attackBar
+    decreaseHp (Monster hpBar attackBar) healthPoints = Monster (hpBar - healthPoints) attackBar
+
+getReplyString :: Int -> String
+getReplyString roundNum = case roundNum `mod` 2 of
+    1 -> " attacked his opponent\n"
+    0 -> " striked his opponent back\n"
+    _ -> error "Something went really wrong"
+
+fighting :: Creature -> Creature -> String
+fighting fCreature@(Knight _ _) sCreature
+    = fight fCreature sCreature "The fight has begun\n" 1
+fighting fCreature sCreature
+    = fight sCreature fCreature "The fight has begun\n" 1
+
+fight :: Creature -> Creature -> String -> Int -> String
+fight fCreature sCreature logFight roundNum =
+    if (getHp sCreature - getAttackPoints fCreature) <= 0
+        then logFight ++ "Round " ++ show roundNum ++ ": The fighter " ++ show fCreature ++ " win!\n"
+        else fight (decreaseHp sCreature $ getAttackPoints fCreature) fCreature
+            (logFight ++ "Round " ++ show roundNum ++ ": The fighter " ++ show fCreature ++ getReplyString roundNum)
+            (roundNum + 1)
+
+
+data Vector a = Vector2D a a | Vector3D a a a deriving (Show, Eq)
+
+to3D :: Num a => Vector a -> Vector a
+to3D (Vector2D x y) = Vector3D x y 0
+to3D vector3D       = vector3D
+
+vecLength :: Floating a => Vector a -> a
+vecLength vector = let (Vector3D x y z) = to3D vector in
+    sqrt $ x ** 2 + y ** 2 + z ** 2
+
+vecSum :: Floating a => Vector a -> Vector a -> Vector a
+vecSum x y = Vector3D (x1 + x2) (y1 + y2) (z1 + z2)
+    where
+        (Vector3D x1 y1 z1) = to3D x
+        (Vector3D x2 y2 z2) = to3D y
+
+vecMul :: Floating a => Vector a -> Vector a -> a
+vecMul x y = x1 * x2 + y1 * y2 + z1 * z2
+    where
+        (Vector3D x1 y1 z1) = to3D x
+        (Vector3D x2 y2 z2) = to3D y
+
+dist :: Floating a => Vector a -> Vector a -> a
+dist x y = sqrt $ (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+    where
+        (Vector3D x1 y1 z1) = to3D x
+        (Vector3D x2 y2 z2) = to3D y
+
+vecMulVect :: Floating a => Vector a -> Vector a -> Vector a
+vecMulVect x y = Vector3D (y1 * z2 - z1 * y2) (z1 * x2 - x1 * z2) (x1 * y2 - y1 * x2)
+    where
+        (Vector3D x1 y1 z1) = to3D x
+        (Vector3D x2 y2 z2) = to3D y
+
+
+data Nat = Z | S Nat deriving (Show)
+
+natSum :: Nat -> Nat -> Nat
+natSum Z x     = x
+natSum (S x) z = natSum x (S z)
+
+natMul :: Nat -> Nat -> Nat
+natMul Z _     = Z
+natMul _ Z     = Z
+natMul (S Z) x = x
+natMul (S x) z = natSum z $ natMul x z
+
+natSub :: Nat -> Nat -> Maybe Nat
+natSub x Z         = Just x
+natSub Z (S _)     = Nothing
+natSub (S x) (S y) = natSub x y
+
+natToNum :: Nat -> Int
+natToNum Z     = 0
+natToNum (S x) = 1 + natToNum x
+
+instance Eq Nat where
+    (==) Z Z         = True
+    (==) Z _         = False
+    (==) _ Z         = False
+    (==) (S x) (S y) = x == y
+
+instance Ord Nat where
+    compare Z Z         = EQ
+    compare Z _         = LT
+    compare _ Z         = GT
+    compare (S x) (S y) = compare x y
+
+isEven :: Nat -> Bool
+isEven x = isEven' x 0
+    where
+        isEven' :: Nat -> Int -> Bool
+        isEven' Z k     = (k `mod` 2) == 0
+        isEven' (S n) k = isEven' n (k + 1)
+
+natDiv :: Nat -> Nat -> Maybe Nat
+natDiv _ Z = Nothing
+natDiv x y = natDiv' x y Z
+    where
+        natDiv' x' y' z' =
+            if x' >= y'
+                then natDiv' (fromJust (natSub x' y')) y' (S z')
+                else Just z'
+
+natMod :: Nat -> Nat -> Maybe Nat
+natMod _ Z = Nothing
+natMod x y =
+    if x >= y
+        then natMod (fromJust (natSub x y)) y
+        else Just x
+
+natGcd :: Nat -> Nat -> Maybe Nat
+natGcd Z _ = Nothing
+natGcd _ Z = Nothing
+natGcd x y = Just $ natGcd' x y
+    where
+        natGcd' x' Z = x'
+        natGcd' x' y' = natGcd' y' (fromJust (natMod x' y'))
+
+
+data Tree a = Leaf | Node a (Tree a) (Tree a) deriving (Show)
+
+isEmptyTree :: Tree a -> Bool
+isEmptyTree Leaf = True
+isEmptyTree _    = False
+
+sizeTree :: Tree a -> Int
+sizeTree Leaf                = 0
+sizeTree (Node _ left right) = 1 + sizeTree left + sizeTree right
+
+findElemTree :: Ord a => Tree a -> a -> Bool
+findElemTree Leaf _ = False
+findElemTree (Node x left right) element =
+    x == element || findElemTree left element || findElemTree right element
+
+insertIntoTree :: Ord a => Tree a -> a -> Tree a
+insertIntoTree Leaf element = Node element Leaf Leaf
+insertIntoTree (Node x left right) element
+    | element > x   = Node x left $ insertIntoTree right element
+    | element < x   = Node x (insertIntoTree left element) right
+    | otherwise     = Node x left right
+
+fromList :: Ord a => [a] -> Tree a
+fromList []     = Leaf
+fromList [x]    = insertIntoTree Leaf x
+fromList (x:xs) = insertIntoTree (fromList xs) x
+
+instance (Eq a) => Eq (Tree a) where
+    (==) Leaf Leaf         = True
+    (==) Leaf _         = False
+    (==) _ Leaf         = False
+    (==) (Node x left right) (Node y left1 right1) = x == y && left == left1 && right == right1
+
+----------Block4
+
+instance Foldable Tree where
+    foldMap _ Leaf = mempty
+    foldMap f (Node x Leaf Leaf) = f x
+    foldMap f (Node x Leaf right) = f x `mappend` foldMap f right
+    foldMap f (Node x left Leaf) = f x `mappend` foldMap f left
+    foldMap f (Node x left right) = f x `mappend `foldMap f left `mappend` foldMap f right
+    foldr _ acc Leaf = acc
+    foldr f acc (Node x Leaf Leaf) = f x acc
+    foldr f acc (Node x Leaf right) = f x $ foldr f acc right
+    foldr f acc (Node x left Leaf) = (\acc' -> foldr f acc' left) $ f x acc
+    foldr f acc (Node x left right) = (\acc' -> foldr f acc' left) $ f x $ foldr f acc right
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn element = foldr (\x acc -> if x == element
+    then [] : acc
+    else let (y:ys) = acc in (x : y) : ys) [[]]
+
+joinWith :: a -> [[a]] -> [a] ----todo
+joinWith _ []         = []
+joinWith element list = init $ foldr (\x acc -> x ++ [element] ++ acc) [] list
+
+---------Block5
+
+maybeConcat :: [Maybe [a]] -> [a]
+maybeConcat []     = []
+maybeConcat [x]    = fromMaybe [] x
+maybeConcat (x:xs) = fromMaybe [] x `mappend` maybeConcat xs
+
+eitherConcat :: (Monoid m, Monoid n) => [Either m n] -> Maybe (m, n)
+eitherConcat list = if null leftElems || null rightElems
+    then Nothing
+    else Just (concatLeft leftElems, concatRight rightElems)
+    where
+        leftElems = lefts list
+        rightElems = rights list
+        concatLeft []       = error "Something went really wrong"
+        concatLeft [x]      = x
+        concatLeft (x : xs) = x `mappend` concatLeft xs
+        concatRight []       = error "Something went really wrong"        
+        concatRight [x]      = x
+        concatRight (x : xs) = x `mappend` concatRight xs
+
+data NonEmpty a = a :| [a] deriving (Show, Eq)
+
+instance Semigroup (NonEmpty a) where
+    (<>) (x :| xs) (y :| ys) = x :| (xs ++ [y] ++ ys)
+
+newtype Identity a = Identity { runIdentity :: a } deriving (Show, Eq)
+
+instance Semigroup a => Semigroup (Identity a) where
+    (<>) x y = Identity $ runIdentity x <> runIdentity y
+
+instance Monoid a => Monoid (Identity a) where
+    mempty = Identity mempty
+    mappend x y = Identity $ runIdentity x `mappend` runIdentity y
+
+
+newtype Name = Name String deriving (Show, Eq)
+
+instance Semigroup Name where
+    (<>) (Name x) (Name y) = Name (x ++ "." ++ y)
+
+instance Monoid Name where
+    mempty = Name []
+    mappend (Name x) (Name y)
+        | null x && null y = mempty
+        | null x = Name y
+        | null y = Name x
+        | otherwise = Name (x ++ "." ++ y)
+
+
+newtype Endo a = Endo { getEndo :: a -> a } 
+
+instance Semigroup (Endo a) where
+    (<>) x y = Endo (getEndo x . getEndo y)
+
+instance Monoid (Endo a) where
+    mempty = Endo id
+    mappend x y = Endo (getEndo x . getEndo y)
+
+newtype Arrow a b = Arrow { getArrow :: a -> b }
+
+instance Semigroup b => Semigroup (Arrow a b) where
+    (<>) x y = Arrow (\l -> getArrow x l <> getArrow y l)
+
+instance Monoid b => Monoid (Arrow a b) where
+    mempty = Arrow mempty
+    mappend x y = Arrow (\l -> getArrow x l `mappend` getArrow y l)
+
+instance Ord a => Semigroup (Tree a) where
+    (<>) x y = foldr (\element tree -> insertIntoTree tree element) x y
+
+instance Ord a => Monoid (Tree a) where
+    mempty = Leaf
+    mappend x Leaf = x
+    mappend Leaf x = x
+    mappend x y = foldr (\element tree -> insertIntoTree tree element) x y
---
+++ hw1/test/TestSpec.hs
@@ -0,0 +1,231 @@
+module TestSpec (spec) where
+
+import           Data.Foldable         (toList)
+import           Data.List             (nub, sort)
+import           Data.Maybe            (isJust, isNothing)
+import           Data.Monoid           (Any(..), Sum(..)) 
+import           Data.Monoid           hiding ((<>), Endo)
+import           Data.Semigroup as Semigroup ((<>))
+import           Test.Hspec
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+
+import           Lib
+
+passTests = [ "1", "1 2 3", " 1", "1 ", "\t1\t", "\t12345\t", "010 020 030" ,
+ " 123 456 789 ", "-1", "-1 -2 -3", "\t-12345\t", " -123 -456 -789 ",
+  "\n1\t\n3   555  -1\n\n\n-5", "123\t\n\t\n\t\n321 -4 -40"]
+
+mustFail  = ["asd", "1-1", "1.2", "--2", "+1", "1+"]
+
+advancedTests    = [ "+1", "1 +1", "-1 +1", "+1 -1"]
+
+advancedMustFail = ["1+1", "++1", "-+1", "+-1", "1 + 1", "1 ++ 1"]
+
+fighting1 = "The fight has begun\nRound 1: The fighter Knight {hp = 10, attack = 5} attacked his opponent\n"
+    ++ "Round 2: The fighter Monster {hp = 5, attack = 5} striked his opponent back\n"
+    ++ "Round 3: The fighter Knight {hp = 5, attack = 5} win!\n"
+
+fighting2 = "The fight has begun\nRound 1: The fighter Knight {hp = 10, attack = 5} attacked his opponent\n"
+    ++ "Round 2: The fighter Knight {hp = 5, attack = 5} striked his opponent back\n"
+    ++ "Round 3: The fighter Knight {hp = 5, attack = 5} win!\n"
+
+fighting3 = "The fight has begun\nRound 1: The fighter Monster {hp = 10, attack = 5} attacked his opponent\n"
+    ++ "Round 2: The fighter Monster {hp = 5, attack = 5} striked his opponent back\n"
+    ++ "Round 3: The fighter Monster {hp = 5, attack = 5} win!\n"
+
+arrToSort = [38,-53,-21,98,-92,-69,-52,1,-89]
+
+tree = (Node (3 :: Int) (Node (1 :: Int) Leaf Leaf) (Node (123 :: Int) (Node (4 :: Int) Leaf Leaf) Leaf))
+treeForMonoidTest = toList $ tree `mappend` (Node 2 Leaf Leaf)
+
+treeFromList = fromList arrToSort
+
+endo1 = Endo (\l -> l * 2)
+endo2 = Endo (\l -> l + 1)
+endo3 = Endo (\l -> l + 3)
+endo4 = (mempty :: Endo Int)
+
+fun1 = getEndo ((endo1 <> endo2) <> endo3)
+fun2 = getEndo (endo1 <> (endo2 <> endo3))
+fun3 = getEndo (endo1 <> endo4)
+
+arrow1 = Arrow (\l -> [[l, l]])
+arrow2 = Arrow (\l -> [[l], [l]])
+arrow3 = Arrow mempty
+
+funArr1 = getArrow (arrow1 `mappend` arrow2)
+funArr2 = getArrow (arrow1 `mappend` arrow3)
+
+spec :: Spec
+spec = do
+    describe "Block1" $ do
+        it "order3" $ do
+            order3 (3, 2, 1) `shouldBe` (1, 2, 3)
+            order3 (2, 3, 1) `shouldBe` (1, 2, 3)
+            order3 (1, 2, 3) `shouldBe` (1, 2, 3)
+        it "highestBit" $ do
+            highestBit 0 `shouldBe` Nothing
+            highestBit 1 `shouldBe` Just 1
+            highestBit 15 `shouldBe` Just 8
+            highestBit 16 `shouldBe` Just 16
+        it "highestBitHard" $ do
+            highestBitHard 0 `shouldBe` Nothing
+            highestBitHard 1 `shouldBe` Just (1, 0)
+            highestBitHard 15 `shouldBe` Just (8, 3)
+            highestBitHard 16 `shouldBe` Just (16, 4)
+        it "smartReplicate" $ do
+            smartReplicate [1, 2, 3] `shouldBe` [1, 2, 2, 3, 3, 3]
+            smartReplicate [1, 2, 0] `shouldBe` [1, 2, 2]
+        it "contains" $ do
+            contains 3 [[1..5], [2,0], [3,4]] `shouldBe` [[1,2,3,4,5],[3,4]]
+            contains 10 [[1..5], [2,0], [3,4]] `shouldBe` []
+    describe "Block2" $ do
+        it "removeAt" $ do
+            removeAt 1 [1,2,3] `shouldBe` Just [1, 3]
+            removeAt 10 [1,2,3] `shouldBe` Just [1,2,3]
+            removeAt 3 [1..5] `shouldBe` Just [1,2,3,5]
+            removeAt 2 "abc" `shouldBe` Just "ab"
+            removeAt (-1) "abc" `shouldBe` Nothing
+        it "removeAtHard" $ do
+            removeAtHard 1 [1,2,3] `shouldBe` (Just 2, [1, 3])
+            removeAtHard 10 [1,2,3] `shouldBe` (Nothing, [1, 2, 3])
+            removeAtHard (-1) [1,2,3] `shouldBe` (Nothing, [1, 2, 3])
+        it "collectEvery" $ do
+            collectEvery 3 [1..8] `shouldBe` Just ([1,2,4,5,7,8], [3,6])
+            collectEvery 10 [1..8] `shouldBe` Just ([1,2,3,4,5,6,7,8], [])
+            collectEvery 0 [1..8] `shouldBe` Nothing
+        it "stringSum" $ do
+            map stringSum passTests `shouldBe` [Just 1, Just 6, Just 1, Just 1, Just 1, Just 12345,
+                Just 60, Just 1368, Just (-1), Just (-6), Just (-12345), Just (-1368), Just 553, Just 400]
+            map stringSum mustFail `shouldBe` [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+        it "stringSumHard" $ do
+            map stringSumHard advancedTests `shouldBe` [Just 1, Just 2, Just 0, Just 0]
+            map stringSumHard advancedMustFail`shouldBe` [Nothing, Nothing, Nothing, Nothing, Nothing, Nothing]
+        it "mergeSort" $ do
+            mergeSort arrToSort `shouldBe` sort arrToSort 
+    describe "Block3" $ do
+        it "nextDay" $ do
+            nextDay Monday `shouldBe` Tuesday
+            nextDay Sunday `shouldBe` Monday
+        it "afterDays" $ do
+            afterDays Monday 5 `shouldBe` Saturday
+            afterDays Saturday 5 `shouldBe` Thursday
+        it "isWeekend" $ do
+            isWeekend Saturday `shouldBe` True
+        it "daysToParty" $ do 
+            daysToParty Friday `shouldBe` 0
+            daysToParty Sunday `shouldBe` 5
+        it "fighting" $ do
+            fighting (Knight 10 5) (Monster 10 5) `shouldBe` fighting1
+            fighting (Knight 10 5) (Knight 10 5) `shouldBe` fighting2
+            fighting (Monster 10 5) (Monster 10 5) `shouldBe` fighting3
+        it "vecLength" $ do
+            vecLength (Vector3D 0.0 0.0 0.0) `shouldBe` 0
+            vecLength (Vector3D 1.0 2.0 3.0) `shouldBe` 3.7416573867739413
+            vecLength (Vector2D 5.0 12.0) `shouldBe` 13
+        it "vecLength" $ do
+            vecSum (Vector3D 1.0 2.0 3.0) (Vector3D 4 5 6) `shouldBe` (Vector3D 5 7 9)
+            vecSum (Vector2D 1 2) (Vector3D 4 5 6) `shouldBe` (Vector3D 5 7 6)
+        it "vecMul" $ do 
+            vecMul (Vector3D 1 2 3) (Vector3D 4 5 6) `shouldBe` 32
+            vecMul (Vector2D 1 2) (Vector3D 4 5 6) `shouldBe` 14
+        it "dist" $ do 
+            dist (Vector3D 0 0 0) (Vector2D 12 5) `shouldBe` 13
+        it "vecMulVect" $ do 
+            vecMulVect (Vector3D 1 2 3) (Vector3D 4 5 6) `shouldBe` (Vector3D (-3) 6 (-3))
+            vecMulVect (Vector3D 1 2 3) (Vector3D 2 4 6) `shouldBe` (Vector3D 0 0 0)
+        it "natSum" $ do 
+            natSum (S (S Z)) (S (S (S Z))) `shouldBe` (S (S (S (S (S Z)))))
+            natSum (S (S Z)) (Z) `shouldBe` (S (S Z))
+        it "natMul" $ do
+            natMul (S (S Z)) (S (S (S Z))) `shouldBe` (S (S (S (S (S (S Z))))))
+            natMul (S (S Z)) (Z) `shouldBe` (Z)
+        it "natSub" $ do
+            natSub (S (S Z)) (S (S (S Z))) `shouldBe` Nothing
+            natSub (S (S Z)) (Z) `shouldBe` Just (S (S Z))
+            natSub (S (S (S Z))) (S (S Z)) `shouldBe` Just (S Z)
+        it "natToNum" $ do
+            natToNum (S (S Z)) `shouldBe` 2
+            natToNum (Z) `shouldBe` 0
+        it "equality" $ do
+            (S (S (S (S Z)))) == (S (S (S Z))) `shouldBe` False
+            (S (S (S (S Z)))) == (S (S (S (S Z)))) `shouldBe` True
+        it "ordering" $ do
+            (S (S (S (S Z)))) < (S (S (S Z))) `shouldBe` False
+            (S (S (S (S Z)))) > (S (S (S Z))) `shouldBe` True
+            (S (S (S (S Z)))) > (S (S (S (S Z)))) `shouldBe` False
+        it "isEven" $ do
+            isEven (S (S (S (S Z)))) `shouldBe` True
+            isEven (S (S (S Z))) `shouldBe` False
+            isEven (Z) `shouldBe` True
+        it "natDiv" $ do
+            natDiv (S (S Z)) (S (S (S Z))) `shouldBe` Just (Z)
+            natDiv (S (S (S Z))) (S (S Z)) `shouldBe` Just (S (Z))
+            natDiv (S (S Z)) (Z) `shouldBe` Nothing
+            natDiv (S (S (S (S (S (S Z)))))) (S (S (S Z))) `shouldBe` Just (S (S Z))
+        it "natMod" $ do
+            natMod (S (S Z)) (S (S (S Z))) `shouldBe` Just (S (S Z))
+            natMod (S (S (S Z))) (S (S Z)) `shouldBe` Just (S (Z))
+            natMod (S (S Z)) (Z) `shouldBe` Nothing
+            natMod (S (S (S (S (S (S Z)))))) (S (S (S Z))) `shouldBe` Just (Z)
+        it "natGcd" $ do
+            natGcd (Z) (S (S Z)) `shouldBe` Nothing
+            natGcd (S (S Z)) (Z) `shouldBe` Nothing
+            natGcd (S (S (S (S (S (S Z)))))) (S (S (S Z))) `shouldBe` Just (S (S (S Z))) 
+        it "isEmptyTree" $ do
+            isEmptyTree (Leaf) `shouldBe` True
+            isEmptyTree tree `shouldBe` False
+        it "findElemTree" $ do
+            findElemTree (Leaf) 1 `shouldBe` False
+            findElemTree tree 1 `shouldBe` True
+            findElemTree tree 2 `shouldBe` False
+        it "sizeTree" $ do
+            sizeTree (Leaf) `shouldBe` 0
+            sizeTree tree `shouldBe` 4
+        it "insertIntoTree" $ do
+            insertIntoTree (Leaf) 2 `shouldBe` (Node (2::Int) Leaf Leaf)
+            insertIntoTree tree 2 `shouldBe` (Node (3::Int) (Node (1::Int) Leaf (Node (2::Int) Leaf Leaf)) $ Node (123::Int) (Node (4::Int) Leaf Leaf) Leaf)
+            insertIntoTree tree 123 `shouldBe` tree
+        it "fromList" $ do
+            fromList ([] :: [Int]) `shouldBe` Leaf
+            fromList [1, 2, 3, 4, 5, 6, 7] `shouldBe` (Node 7 (Node 6 (Node 5 (Node 4 (Node 3 (Node 2 (Node 1 Leaf Leaf) Leaf) Leaf) Leaf) Leaf) Leaf) Leaf)
+    describe "Block4" $ do
+        it "foldMapTree" $ do 
+            foldMap (Any . (== 1)) tree `shouldBe` (Any {getAny = True})
+            foldMap (Any . (== 2)) tree `shouldBe` (Any {getAny = False})
+        it "foldrTree" $ do
+            toList treeFromList `shouldBe` sort arrToSort
+        it "splitOn" $ do 
+            splitOn '/' "path/to/file" `shouldBe` ["path", "to", "file"]
+            splitOn '/' "//" `shouldBe` ["", "", ""]
+        it "joinWith" $ do 
+            joinWith '/' ["path", "to", "file"] `shouldBe` "path/to/file"
+            joinWith '/' ["", "", "", ""] `shouldBe` "///"
+    describe "Block5" $ do 
+        it "maybeConcat" $ do
+            maybeConcat [Just [1,2,3], Nothing, Just [4,5]] `shouldBe` [1, 2, 3, 4, 5]
+            maybeConcat ([Nothing] :: [Maybe [Int]]) `shouldBe` ([] :: [Int]) 
+        it "eitherConcat" $ do
+            eitherConcat [Left (Sum 3), Right [1,2,3], Left (Sum 5), Right [4,5]] `shouldBe` Just (Sum {getSum = 8}, [1,2,3,4,5])
+            eitherConcat ([Left (Sum 3), Left (Sum 5)] :: [Either (Sum Integer) [Int]]) `shouldBe` Nothing
+        it "Semigroup.NonEmpty" $ do 
+            ((1 :| [2, 3, 4]) :: NonEmpty Int) <> ((5 :| [6, 7, 8]) :: NonEmpty Int) `shouldBe` ((1 :| [2, 3, 4, 5, 6, 7, 8]) :: NonEmpty Int)
+        it "Monoid.Identity" $ do 
+            (Identity [1]) `mappend` (Identity [2]) `shouldBe` (Identity [1, 2])
+            (Identity [1]) `mappend` (Identity ([] :: [Int])) `shouldBe` (Identity [1])
+        it "Monoid.Name" $ do 
+            (Name "root") <> (Name "server") `shouldBe` (Name "root.server")
+            (Name "root") `mappend` (Name "") `shouldBe` (Name "root")
+            (Name "") `mappend` (Name "server") `shouldBe` (Name "server")
+        it "Monoid.Endo" $ do 
+            fun1 1 `shouldBe` 10
+            fun2 1 `shouldBe` 10
+            fun3 1 `shouldBe` 2
+        it "Monoid.Arrow" $ do
+            funArr1 1 `shouldBe` [[1, 1], [1], [1]]
+            funArr2 1 `shouldBe` [[1, 1]]
+        it "MonoidTree" $ do 
+            tree <> tree `shouldBe` tree
+            Leaf `mappend` tree `shouldBe` tree
+            treeForMonoidTest `shouldBe` [1, 2, 3, 4, 123]
