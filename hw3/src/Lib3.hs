99fe5603ca7c9ddc3646e37e94225410f8a0e8b2
fmixing <alicekoroleva239@gmail.com>
2017-11-24 01:56:09 +0300

    3, 4, 5, 6 tasks seem to be done

--- hw3/src/Lib3.hs
+++ hw3/src/Lib3.hs
@@ -7,6 +7,9 @@
 {-# LANGUAGE FlexibleInstances         #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Lib3
        (
@@ -18,13 +21,30 @@ module Lib3
        , whileParser
        , AExpr (..)
        , ABinOp (..)
+       , StateEnvironment (..)
+       , StateError (..)
+       , declare
+       , update
+       , Variable (..)
+       , parseVarExpr
+       , evaluateVarExpr
+       , evaluateVarExpr'
+       , main
+       , ParsingError (..)
        ) where
 
-import qualified Data.Map.Strict as Map (Map, fromList, insert, lookup)
+import qualified Data.Map.Strict as Map (Map, fromList, insert, lookup, empty)
 import Control.Applicative (liftA2)
-import Control.Monad.Reader (Reader, asks, runReader)
+import Control.Monad.Reader (Reader, asks, runReader, MonadReader)
+import Control.Monad.State (State, get, state, runState, liftM, MonadState, MonadIO, StateT, evalState, evalStateT, execStateT)
 import Control.Monad (fail)
-import Data.Maybe (maybe)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Trans.Maybe (runMaybeT)
+import Data.Maybe (maybe, isJust, isNothing)
+import Data.Either (isLeft, either)
+import Debug.Trace (trace)
+import Data.List (null)
+import Control.Monad (void)
 
 import Data.Void (Void)
 import Text.Megaparsec (Parsec, notFollowedBy, between, try, many, (<|>), parseTest', parseTest, eof, parse)
@@ -32,45 +52,60 @@ import Text.Megaparsec.Char (space, space1, char, string, alphaNumChar, letterCh
 import Text.Megaparsec.Expr (Operator (..), makeExprParser)
 import qualified Text.Megaparsec.Char.Lexer as L
 
-
+----------------First
 
 type VarName = String
 type Const = Integer
 data Expr = Var VarName | Lit Const | Add Expr Expr | 
             Mul Expr Expr | Sub Expr Expr | Div Expr Expr | 
-            Let VarName Expr Expr deriving (Show, Eq)
+            Let VarName Expr Expr | Neg Expr deriving (Show, Eq)
 
 data Environment = Environment { dict  :: Map.Map VarName Const }
 
+data ParsingError = ArithmError ArithmeticError | VarError StateError deriving (Show, Eq)
+
 data ArithmeticError = DivisionByZero | NameNotFound deriving (Show, Eq)
 
-getVarValue :: VarName -> Reader Environment (Either ArithmeticError Const)
-getVarValue varName = asks ((maybe (Left NameNotFound) (Right . id)) . Map.lookup varName . dict)
+getVarValue :: VarName -> Reader Environment (Either ParsingError Const)
+getVarValue varName = asks ((maybe (Left $ ArithmError $ NameNotFound) (Right . id)) . Map.lookup varName . dict)
 
-evaluateInternal :: Expr -> Expr -> (Const -> Const -> Const) -> Reader Environment (Either ArithmeticError Const)
+evaluateInternal :: Expr -> Expr -> (Const -> Const -> Const) -> Reader Environment (Either ParsingError Const)
 evaluateInternal x y func = asks (\env -> 
     let resx = runReader (evaluate x) env
         resy = runReader (evaluate y) env
     in liftA2 func resx resy)
 
-evaluate :: Expr -> Reader Environment (Either ArithmeticError Integer)
+evaluate :: Expr -> Reader Environment (Either ParsingError Integer)
 evaluate (Lit x) = return $ Right x
 evaluate (Var name) = getVarValue name
+evaluate (Neg x) = evaluateInternal (Lit 0) x (-)
 evaluate (Add x y) = evaluateInternal x y (+)
 evaluate (Mul x y) = evaluateInternal x y (*)
 evaluate (Sub x y) = evaluateInternal x y (-)
 evaluate (Div x y) = asks (\env -> let resy = runReader (evaluate y) env in 
-    resy >>= (\res -> if res == 0 then Left DivisionByZero else liftA2 (div) (runReader (evaluate x) env) resy))
+    resy >>= (\res -> if res == 0 then Left $ ArithmError $ DivisionByZero else liftA2 (div) (runReader (evaluate x) env) resy))
 evaluate (Let name y z) = asks (\env -> let resy = runReader (evaluate y) env in 
     resy >>= (\res -> runReader (evaluate z) (Environment $ Map.insert name res $ dict env)))
 
+
+----------------Second
+
+convertAExprToExpr :: AExpr -> Expr
+convertAExprToExpr (AVar varName) = Var varName
+convertAExprToExpr (ALit aConst) = Lit aConst
+convertAExprToExpr (ANeg eExpr) = Neg $ convertAExprToExpr eExpr
+convertAExprToExpr (ABinary ASub aExpr1 aExpr2) = Sub (convertAExprToExpr aExpr1) (convertAExprToExpr aExpr2)
+convertAExprToExpr (ABinary AAdd aExpr1 aExpr2) = Add (convertAExprToExpr aExpr1) (convertAExprToExpr aExpr2)
+convertAExprToExpr (ABinary AMul aExpr1 aExpr2) = Mul (convertAExprToExpr aExpr1) (convertAExprToExpr aExpr2)
+convertAExprToExpr (ABinary ADiv aExpr1 aExpr2) = Div (convertAExprToExpr aExpr1) (convertAExprToExpr aExpr2)
+convertAExprToExpr (ALet varName aExpr1 aExpr2) = Let varName (convertAExprToExpr aExpr1) (convertAExprToExpr aExpr2)
 
 
 -- x + 3 * (let x = 2 in x / 2)
 
 data AExpr = AVar VarName
            | ALit Const
-           | Neg AExpr
+           | ANeg AExpr
            | ABinary ABinOp AExpr AExpr
            | ALet VarName AExpr AExpr
            deriving (Show, Eq)
@@ -80,7 +115,6 @@ data ABinOp = AAdd
             | AMul
             | ADiv
             deriving (Show, Eq)
-
 
 -- data ParsecT e s m a
 -- e - error, s - stream type, m - underlying monad, a - return type
@@ -101,7 +135,7 @@ aTerm = parens letParser
 
 aOperators :: [[Operator Parser AExpr]]
 aOperators = 
-    [[ Prefix  (Neg <$ symbol "-"), 
+    [[ Prefix  (ANeg <$ symbol "-"), 
        InfixL  ((ABinary AMul) <$ symbol "*"),
        InfixL  ((ABinary ADiv) <$ symbol "/")],
      [ InfixL  ((ABinary AAdd) <$ symbol "+"),
@@ -125,8 +159,8 @@ varNameParserS = (lexeme . try) (parseName >>= checkName)
     parseName :: Parser String
     parseName = (:) <$> letterChar <*> many alphaNumChar --проверяет, что сначала буква, потом [a..zA..Z0..9]*
     checkName :: String -> Parser String
-    checkName varName = if (varName == "let") || (varName ==  "in") --проверка на то, что слово не зарезервировано
-        then fail $ "Name of var should be neither let nor in" 
+    checkName varName = if (varName == "let") || (varName ==  "in" || varName == "mut") --проверка на то, что слово не зарезервировано
+        then fail $ "Name of var should be neither let nor in, nor mut" 
         else return varName
 
 rword :: String -> Parser ()
@@ -142,3 +176,136 @@ letParser = do
     sExpr <- aExpr
     return (ALet x expr sExpr)
 
+
+----------------Third
+
+data StateEnvironment = StateEnvironment { stateDict :: Map.Map String Integer } deriving (Show)
+
+data StateError = NameAlreadyExistsState | NameNotFoundState deriving (Show, Eq)
+
+declare :: (MonadState StateEnvironment m) => String -> Integer -> m (Maybe ParsingError)
+declare name value = state $ (\env -> let mapEnv = stateDict env in 
+                     if isJust $ Map.lookup name mapEnv
+                     then (Just $ VarError NameAlreadyExistsState, env)
+                     else (Nothing, StateEnvironment $ Map.insert name value mapEnv))
+
+update :: (MonadState StateEnvironment m) => String -> Integer -> m (Maybe ParsingError)
+update name value = state $ (\env -> let mapEnv = stateDict env in 
+                     if isNothing $ Map.lookup name mapEnv
+                     then (Just $ VarError NameNotFoundState, env)
+                     else (Nothing, StateEnvironment $ Map.insert name value mapEnv))
+
+---------------Fourth
+
+data Variable = Declare VarName AExpr | Update VarName AExpr | Console AExpr deriving (Show, Eq)
+
+isDeclare :: Variable -> Bool
+isDeclare (Declare _ _) = True
+isDeclare _ = False
+
+isConsole :: Variable -> Bool
+isConsole (Console _) = True
+isConsole _ = False
+
+getVarName :: Variable -> VarName
+getVarName (Declare varName _) = varName
+getVarName (Update varName _) = varName
+getVarName (Console _) = error "Console expr has no varName"
+
+getVarExpr :: Variable -> AExpr
+getVarExpr (Declare _ varExpr) = varExpr
+getVarExpr (Update _ varExpr) = varExpr
+getVarExpr (Console varExpr) = varExpr
+ 
+parseVarExpr :: Parser Variable
+parseVarExpr = between space eof variable
+
+variable :: Parser Variable
+variable = declarationParser
+    <|> updatingParser
+    <|> consoleParser
+
+declarationParser :: Parser Variable
+declarationParser = do
+    rword "mut"
+    x <- varNameParserS
+    rword "="
+    expr <- aExpr
+    return (Declare x expr)
+
+updatingParser :: Parser Variable
+updatingParser = do
+    x <- varNameParserS
+    rword "="
+    expr <- aExpr
+    return (Update x expr)
+
+consoleParser :: Parser Variable
+consoleParser = do
+    rword "<"
+    expr <- aExpr
+    return (Console expr)
+
+---------------Fifth&Sixth
+
+fromLeft :: Either a b -> a
+fromLeft (Left a) = a
+fromLeft _ = error "I sense something bad"
+
+fromRight :: Either a b -> b
+fromRight (Right b) = b
+fromRight _ = error "I sense something bad"
+
+printConsole :: (MonadState StateEnvironment m, MonadIO m) => Variable -> m (Maybe ParsingError)
+printConsole x = do 
+    let xExpr = getVarExpr x
+    stateEnv <- get
+    let xVal = runReader (evaluate $ convertAExprToExpr xExpr) (Environment $ stateDict stateEnv)
+    liftIO $ putStrLn $ "< value for expression " ++ show xExpr ++ " is " ++ show xVal
+    return $ either (\err -> Just err) (\_ -> Nothing) xVal
+
+evaluateVarExprInternal :: (MonadState StateEnvironment m, MonadIO m) => Variable -> m (Maybe ParsingError)
+evaluateVarExprInternal x = do
+    let xExpr = getVarExpr x
+    let xName = getVarName x
+    stateEnv <- get
+    let xVal = runReader (evaluate $ convertAExprToExpr xExpr) (Environment $ stateDict stateEnv)
+    ans <- case isLeft xVal of
+        True -> return $ Just $ fromLeft xVal
+        False -> case isDeclare x of 
+            True -> declare xName $ fromRight xVal
+            False -> update xName $ fromRight xVal
+    return ans
+
+---ONLY FOR TESTS
+evaluateVarExpr :: [Variable] -> State StateEnvironment (Maybe ParsingError)
+evaluateVarExpr [] = return Nothing
+evaluateVarExpr (x:xs) = do
+    let xName = getVarName x
+    let xExpr = getVarExpr x
+    stateEnv <- get
+    let xVal = runReader (evaluate $ convertAExprToExpr xExpr) (Environment $ stateDict stateEnv)
+    ans <- case isLeft xVal of
+        True -> return $ Just $ fromLeft xVal
+        False -> case isDeclare x of 
+            True -> declare xName $ fromRight xVal
+            False -> update xName $ fromRight xVal
+    if (not $ null xs) && (isNothing ans) then evaluateVarExpr xs else return ans
+
+evaluateVarExpr' :: (MonadState StateEnvironment m, MonadIO m) => [Variable] -> m ()
+evaluateVarExpr' [] = return ()
+evaluateVarExpr' (x:xs) = do
+    ans <- if isConsole x 
+        then printConsole x
+        else evaluateVarExprInternal x
+    if (not $ null xs) then evaluateVarExpr' xs else liftIO $ print ans 
+
+main :: IO ()
+main = do
+    let upd = Update "x" (ALet "x" (ALit 2) (AVar "x")) 
+    let dec = Declare "x" (ALit 1)
+    let bad = Declare "x" (ABinary AAdd (AVar "x") (ABinary ADiv (ABinary AMul (ALit 3) (ALit 2)) (ALit 0)))
+    let console = Console (ABinary AAdd (AVar "y") (ALit 1))
+    -- putStrLn $ show (runState (evaluateVarExpr [dec, dec]) (StateEnvironment $ Map.empty))
+    ans <- execStateT (evaluateVarExpr' [dec, console]) (StateEnvironment $ Map.empty)
+    print ans
--- hw3/test/TestSpec.hs
+++ hw3/test/TestSpec.hs
@@ -6,12 +6,14 @@ import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
 import           Data.Char
 
-import qualified Data.Map.Strict as Map (fromList)
+import qualified Data.Map.Strict as Map (Map, fromList, empty)
 import           Lib3
 import Control.Monad.Reader (runReader)
 import Text.Megaparsec (parse)
 import Text.Megaparsec.Error (ParseError (..))
 import Data.Either (isLeft)
+import Data.Maybe (isNothing, isJust)
+import Control.Monad.State (State, get, modify, state, runState)
 
 
 evaluateRight = Var "x" `Add` (Lit 3 `Mul` ("x" `Let` (Div (Lit 5) (Lit 2)) $ Var "x"))
@@ -19,15 +21,46 @@ evaluateWrong = Var "x" `Add` (Lit 3 `Mul` ("x" `Let` (Div (Lit 5) (Lit 0)) $ Va
 
 parseRight = ABinary AAdd (AVar "x") (ABinary AMul (ALit 3) (ALet "x" (ABinary AMul (AVar "y") (ALit 3)) (ABinary ADiv (AVar "x") (ALit 2))))
 
+dec = Declare "x" (ALit 1)
+upd = Update "x" (ALet "x" (ALit 2) (AVar "x"))
+bad = Declare "x" (ABinary AAdd (AVar "x") (ABinary ADiv (ABinary AMul (ALit 3) (ALit 2)) (ALit 0)))
+
+prop_declare :: Bool
+prop_declare = ans1 && ans2
+  where
+    ans1 = let ans = runState (declare "y" 1) (StateEnvironment $ Map.fromList [("x", 1)]) in isNothing $ fst ans 
+    ans2 = let ans = runState (declare "y" 1) (StateEnvironment $ Map.fromList [("y", 1)]) in isJust $ fst ans
+    
+prop_parse_var :: Bool
+prop_parse_var = ans1 && ans2 && ans3 && ans4
+  where
+    ans1 = let ans = runState (evaluateVarExpr [dec, upd]) (StateEnvironment $ Map.empty) in isNothing $ fst ans 
+    ans2 = let ans = runState (evaluateVarExpr [upd, dec]) (StateEnvironment $ Map.empty) in isJust $ fst ans
+    ans3 = let ans = runState (evaluateVarExpr [bad, upd]) (StateEnvironment $ Map.empty) in isJust $ fst ans
+    ans4 = let ans = runState (evaluateVarExpr [dec, dec]) (StateEnvironment $ Map.empty) in isJust $ fst ans
+    
 spec :: Spec
 spec = do
     describe "ArithmExpr" $ do
         it "evaluate" $ do
             runReader (evaluate evaluateRight) (Environment $ Map.fromList [("x", 1)]) `shouldBe` Right 7
-            runReader (evaluate evaluateWrong) (Environment $ Map.fromList [("x", 1)]) `shouldBe` Left DivisionByZero
-            runReader (evaluate evaluateRight) (Environment $ Map.fromList [("y", 1)]) `shouldBe` Left NameNotFound
+            runReader (evaluate evaluateWrong) (Environment $ Map.fromList [("x", 1)]) `shouldBe` Left (ArithmError DivisionByZero)
+            runReader (evaluate evaluateRight) (Environment $ Map.fromList [("y", 1)]) `shouldBe` Left (ArithmError NameNotFound)
+            runReader (evaluate evaluateRight) (Environment $ Map.fromList []) `shouldBe` Left (ArithmError NameNotFound)
     describe "Parse" $ do
         it "whileParser" $ do
             parse whileParser "fileName" "x + 3 * (let x = y * 3 in x / 2)" `shouldBe` Right parseRight
             parse whileParser "fileName" "let x = 2 in x" `shouldSatisfy` isLeft
             parse whileParser "fileName" "(let x = 2 in x" `shouldSatisfy` isLeft
+    describe "Declare and update" $ do
+        it "declare" $ prop_declare
+    describe "Parse Var" $ do
+        it "parseVarExpr" $ do
+            parse parseVarExpr "fileName" "mut x = 1" `shouldBe` Right (Declare "x" (ALit 1))
+    describe "blabla" $ do
+        it "eval" $ prop_parse_var
+        -- it "eval" $ do
+        --     runState (evaluateVarExpr [dec, upd]) (StateEnvironment $ Map.empty) `shouldSatisfy` isNothing
+        --     runState (evaluateVarExpr [upd, dec]) (StateEnvironment $ Map.empty) `shouldSatisfy` isJust
+        --     runState (evaluateVarExpr [bad, upd]) (StateEnvironment $ Map.empty) `shouldSatisfy` isJust
+        --     runState (evaluateVarExpr [dec, dec]) (StateEnvironment $ Map.empty) `shouldSatisfy` isJust
